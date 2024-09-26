4f0f629f130b0a731387bd23996fc39df87db681
elias <emairam22@fpcoruna.afundacion.org>
2024-09-26 13:42:36 +0200

    Cabecera semifuncional

--- duacodeapp/src/js/cabecera.js
+++ duacodeapp/src/js/cabecera.js
@@ -2,41 +2,61 @@ import '../css/cabecera.css';
 import logo from '../imagenes/logo-duacode-negro.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
 
+const Cabecera = () => {
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
 
-function Cabecera (){
+  const toggleSubmenu = () => {
+    setIsSubmenuOpen(prevState => !prevState);
+  };
 
-    return(
-        <header className="header">
-        <div className="header-top">
-          <div className="div-logo">
-            <a href="/" className='nav-url'><img src={logo} alt="Logo" className='logo'/></a>
-          </div>
-          <div className="icon">
-            <a href="/ajustes" className='nav-url'><FontAwesomeIcon icon={faBars} className='icono'/></a>
+  return (
+    <header className="header">
+      <div className="header-top">
+        <div className="div-logo">
+          <a href="/" className='nav-url'>
+            <img src={logo} alt="Logo" className='logo' />
+          </a>
+        </div>
+        <div className="icon">
+          <a href="/ajustes" className='nav-url'>
+            <FontAwesomeIcon icon={faBars} className='icono' />
+          </a>
+        </div>
+      </div>
+
+      <nav className="nav-menu">
+        <div className="menu-item">
+          <a href="/*" className='nav-url'>Empleados</a>
+        </div>
+        {/* Empresa con submenú */}
+        <div className={`menu-item-empresa ${isSubmenuOpen ? 'bold' : ''}`} onClick={toggleSubmenu}>
+          <span>Empresa</span>
+          <div className={`submenu ${isSubmenuOpen ? 'active' : ''}`}>
+            <div className="submenu-item">
+              <a href='/*' className='nav-url'>Eventos</a>
+            </div>
+            <div className="submenu-item">
+              <a href='/*' className='nav-url'>Noticias y comunicados</a>
+            </div>
+            <div className="submenu-item">
+              <a href='/*' className='nav-url'>Proyectos y clientes</a>
+            </div>
           </div>
         </div>
-  
-        <nav className="nav-menu">
-          <div className="menu-item">
-          <a href="/*" className='nav-url'>Empleados</a>
-          </div>
-          <div className="menu-item">
-          <a href="/*" className='nav-url'>Empresa</a>
-          </div>
-          <div className="menu-item">
+        <div className="menu-item">
           <a href="/*" className='nav-url'>Distribución de salas e Info</a>
-          </div>
-          <div className="menu-item">
+        </div>
+        <div className="menu-item">
           <a href="/*" className='nav-url'>Calendario</a>
-          </div>
-          <div className="menu-item">
+        </div>
+        <div className="menu-item">
           <a href="/*" className='nav-url'>Protocolos</a>
-          </div>
-        </nav>
-      </header>
-    );
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-    
-}
 export default Cabecera;
